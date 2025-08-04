7ca833b19d1f6342332df9e61af46017da7e3c2e
LuisMaria-Laudadio <luismarialaudadio@gmail.com>
2025-08-04 17:51:31 -0300

    Responsividad

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,38 @@
 import { Link, useLocation } from 'react-router-dom';
+import {useState} from 'react';
 import './Navbar.css';
 
 export default function Navbar() {
   const location = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <ul>
+      {/* Botón hamburguesa */}
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul className={menuOpen ? 'active' : ''}>
         {location.pathname !== '/' && (
-          <li><Link to="/">Inicio</Link></li>
+          <li>
+            <Link to="/" onClick={() => setMenuOpen(false)}>Inicio</Link>
+          </li>
         )}
-        <li><Link to="/sedes">Nuestras sedes</Link></li>
-        <li><Link to="/profesionales">Profesionales</Link></li>
-        <li><Link to="/Planes">Planes</Link></li>
-        <li><button className="buy-button">Asociate</button></li>
+        <li>
+          <Link to="/sedes" onClick={() => setMenuOpen(false)}>Nuestras sedes</Link>
+        </li>
+        <li>
+          <Link to="/profesionales" onClick={() => setMenuOpen(false)}>Profesionales</Link>
+        </li>
+        <li>
+          <Link to="/Planes" onClick={() => setMenuOpen(false)}>Planes</Link>
+        </li>
+        <li>
+          <button className="buy-button">Asociate</button>
+        </li>
       </ul>
     </nav>
   );
